77877c90d16eb7e1fede6a8bb38504562d493bc7
Vuthy Seng <sengvuthy1414@gmail.com>
2026-06-12 20:17:03 +0700

    fix: declare UserRoleListComponent and UserRoleFormComponent properly

--- src/app/components/user-role/user-role.module.ts
+++ src/app/components/user-role/user-role.module.ts
@@ -1,12 +1,18 @@
+//user-role.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { UserRoleRoutingModule } from './user-role-routing.module';
 import { UserRoleListComponent } from './user-role-list/user-role-list.component';
 import { UserRoleFormComponent } from './user-role-form/user-role-form.component';
-import { RouterModule } from '@angular/router';
+import { UserRoleComponent } from './user-role.component';
 
 @NgModule({
+  declarations: [
+    UserRoleComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
