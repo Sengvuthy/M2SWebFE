762dc822d1d078ba34f77fd76b07f4bed07c1390
Vuthy Seng <sengvuthy1414@gmail.com>
2026-06-15 10:17:35 +0700

    Fix CustomerService.login to use environment.apiBaseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://m2swebbe.onrender.com/api',
   backendUrl: 'https://m2swebbe.onrender.com',
-  visionApiKey: 'YOUR_KEY'
+  visionApiKey: 'AIzaSyCK0qTBnHlwWE-vC2G6IxGQBK4PBG8sBQg'
 };
